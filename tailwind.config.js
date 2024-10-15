03855eb6312fe9654c87f1868ed35e4c62c7feec
Haisyam <bersholawat2019@gmailcom>
2024-10-15 23:02:34 +0700

    add page home tugas all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./tugas/index.html"],
+  content: ["./index.html", "./tugas/*.html"],
   theme: {
     container: {
       center: true,
